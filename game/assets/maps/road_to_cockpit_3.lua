559f4b57a013f38af5de558b24aca2d9b9b07c9f
れみろう <remyroez+github@gmail.com>
2018-03-02 21:52:17 +0900

    :+1: 「こっくぴっとへのみち３」にマップ移動イベントを実装

--- game/assets/maps/road_to_cockpit_3.lua
+++ game/assets/maps/road_to_cockpit_3.lua
@@ -8,7 +8,7 @@ return {
   height = 15,
   tilewidth = 16,
   tileheight = 16,
-  nextobjectid = 1,
+  nextobjectid = 3,
   properties = {
     ["background_tile"] = 84,
     ["bgm"] = "space",
@@ -102,6 +102,54 @@ return {
         256, 0, 0, 0, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 0, 256,
         256, 0, 0, 256, 256, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 256,
         256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256
+      }
+    },
+    {
+      type = "objectgroup",
+      name = "object",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      draworder = "topdown",
+      properties = {},
+      objects = {
+        {
+          id = 1,
+          name = "road_to_cockpit_2",
+          type = "transfer",
+          shape = "rectangle",
+          x = 80,
+          y = 224,
+          width = 16,
+          height = 16,
+          rotation = 0,
+          gid = 242,
+          visible = true,
+          properties = {
+            ["transfer_map"] = "road_to_cockpit_2",
+            ["transfer_x"] = 12,
+            ["transfer_y"] = 26
+          }
+        },
+        {
+          id = 2,
+          name = "road_to_cockpit_4",
+          type = "transfer",
+          shape = "rectangle",
+          x = 80,
+          y = 32,
+          width = 16,
+          height = 16,
+          rotation = 0,
+          gid = 242,
+          visible = true,
+          properties = {
+            ["transfer_map"] = "road_to_cockpit_4",
+            ["transfer_x"] = 14,
+            ["transfer_y"] = 1
+          }
+        }
       }
     }
   }
