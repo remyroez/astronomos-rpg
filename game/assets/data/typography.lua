e2e537e0cf2a146b9dced61a8366be28f886a534
れみろう <remyroez+github@gmail.com>
2018-03-05 23:11:55 +0900

    :sparkles: 文字の表示方法を typography ファイルとして分離

---
+++ game/assets/data/typography.lua
@@ -0,0 +1,314 @@
+local voiced_dot = { index = 158, y = -1 }
+local semi_voiced_dot = { index = 159, y = -1 }
+
+return {
+  ["力"] = {
+    index = 1,
+  },
+  ["button"] = {
+    index = 2,
+  },
+  ["right_vertical_line"] = {
+    index = 3,
+  },
+  ["left_vertical_line"] = {
+    index = 4,
+  },
+  ["over_line"] = {
+    index = 5,
+  },
+  ["vertical_frame"] = {
+    index = 6,
+  },
+  ["horizontal_frame"] = {
+    index = 7,
+  },
+  ["left_top_frame"] = {
+    index = 8,
+  },
+  ["right_top_frame"] = {
+    index = 9,
+  },
+  ["left_bottom_frame"] = {
+    index = 10,
+  },
+  ["right_bottom_frame"] = {
+    index = 11,
+  },
+  ["gauge"] = {
+    index = 12,
+  },
+  ["(c)"] = {
+    index = 14,
+  },
+  ["fin"] = {
+    index = 15,
+  },
+  ["cursor"] = {
+    index = 16,
+  },
+  ["up"] = {
+    index = 24,
+  },
+  ["down"] = {
+    index = 25,
+  },
+  ["right"] = {
+    index = 26,
+  },
+  ["left"] = {
+    index = 27,
+  },
+  ["　"] = {
+    index = 32,
+  },
+  ["。"] = {
+    index = 97,
+  },
+  ["、"] = {
+    index = 100,
+  },
+  ["を"] = {
+    index = 102,
+  },
+  ["ぁ"] = {
+    index = 103,
+  },
+  ["ぃ"] = {
+    index = 104,
+  },
+  ["ぅ"] = {
+    index = 105,
+  },
+  ["ぇ"] = {
+    index = 106,
+  },
+  ["ぉ"] = {
+    index = 107,
+  },
+  ["ゃ"] = {
+    index = 108,
+  },
+  ["ゅ"] = {
+    index = 109,
+  },
+  ["ょ"] = {
+    index = 110,
+  },
+  ["っ"] = {
+    index = 111,
+  },
+  ["ー"] = {
+    index = 112,
+  },
+  ["あ"] = {
+    index = 113,
+  },
+  ["い"] = {
+    index = 114,
+  },
+  ["う"] = {
+    index = 115,
+  },
+  ["え"] = {
+    index = 116,
+  },
+  ["お"] = {
+    index = 117,
+  },
+  ["か"] = {
+    index = 118,
+  },
+  ["き"] = {
+    index = 119,
+  },
+  ["く"] = {
+    index = 120,
+  },
+  ["け"] = {
+    index = 121,
+  },
+  ["こ"] = {
+    index = 122,
+  },
+  ["が"] = {
+    { index = 118, }, voiced_dot
+  },
+  ["ぎ"] = {
+    { index = 119, }, voiced_dot
+  },
+  ["ぐ"] = {
+    { index = 120, }, voiced_dot
+  },
+  ["げ"] = {
+    { index = 121, }, voiced_dot
+  },
+  ["ご"] = {
+    { index = 122, }, voiced_dot
+  },
+  ["さ"] = {
+    index = 123,
+  },
+  ["し"] = {
+    index = 124,
+  },
+  ["す"] = {
+    index = 125,
+  },
+  ["せ"] = {
+    index = 126,
+  },
+  ["そ"] = {
+    index = 127,
+  },
+  ["ざ"] = {
+    { index = 123, }, voiced_dot
+  },
+  ["じ"] = {
+    { index = 124, }, voiced_dot
+  },
+  ["ず"] = {
+    { index = 125, }, voiced_dot
+  },
+  ["ぜ"] = {
+    { index = 126, }, voiced_dot
+  },
+  ["ぞ"] = {
+    { index = 127, }, voiced_dot
+  },
+  ["た"] = {
+    index = 128,
+  },
+  ["ち"] = {
+    index = 129,
+  },
+  ["つ"] = {
+    index = 130,
+  },
+  ["て"] = {
+    index = 131,
+  },
+  ["と"] = {
+    index = 132,
+  },
+  ["だ"] = {
+    { index = 128, }, voiced_dot
+  },
+  ["ぢ"] = {
+    { index = 129, }, voiced_dot
+  },
+  ["づ"] = {
+    { index = 130, }, voiced_dot
+  },
+  ["で"] = {
+    { index = 131, }, voiced_dot
+  },
+  ["ど"] = {
+    { index = 132, }, voiced_dot
+  },
+  ["な"] = {
+    index = 133,
+  },
+  ["に"] = {
+    index = 134,
+  },
+  ["ぬ"] = {
+    index = 135,
+  },
+  ["ね"] = {
+    index = 136,
+  },
+  ["の"] = {
+    index = 137,
+  },
+  ["は"] = {
+    index = 138,
+  },
+  ["ひ"] = {
+    index = 139,
+  },
+  ["ふ"] = {
+    index = 140,
+  },
+  ["へ"] = {
+    index = 141,
+  },
+  ["ほ"] = {
+    index = 142,
+  },
+  ["ば"] = {
+    { index = 138, }, voiced_dot
+  },
+  ["び"] = {
+    { index = 139, }, voiced_dot
+  },
+  ["ぶ"] = {
+    { index = 140, }, voiced_dot
+  },
+  ["べ"] = {
+    { index = 141, }, voiced_dot
+  },
+  ["ぼ"] = {
+    { index = 142, }, voiced_dot
+  },
+  ["ぱ"] = {
+    { index = 138, }, semi_voiced_dot
+  },
+  ["ぴ"] = {
+    { index = 139, }, semi_voiced_dot
+  },
+  ["ぷ"] = {
+    { index = 140, }, semi_voiced_dot
+  },
+  ["ぺ"] = {
+    { index = 141, }, semi_voiced_dot
+  },
+  ["ぽ"] = {
+    { index = 142, }, semi_voiced_dot
+  },
+  ["ま"] = {
+    index = 143,
+  },
+  ["み"] = {
+    index = 144,
+  },
+  ["む"] = {
+    index = 145,
+  },
+  ["め"] = {
+    index = 146,
+  },
+  ["も"] = {
+    index = 147,
+  },
+  ["や"] = {
+    index = 148,
+  },
+  ["ゆ"] = {
+    index = 149,
+  },
+  ["よ"] = {
+    index = 150,
+  },
+  ["ら"] = {
+    index = 151,
+  },
+  ["り"] = {
+    index = 152,
+  },
+  ["る"] = {
+    index = 153,
+  },
+  ["れ"] = {
+    index = 154,
+  },
+  ["ろ"] = {
+    index = 155,
+  },
+  ["わ"] = {
+    index = 156,
+  },
+  ["ん"] = {
+    index = 157,
+  },
+}
