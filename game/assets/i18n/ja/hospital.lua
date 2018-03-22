d06f73f138154b7b86f27901a7fcea0f1d0b70cc
れみろう <remyroez+github@gmail.com>
2018-03-22 21:52:53 +0900

    :sparkles: 「びょういん」にNPC設定

---
+++ game/assets/i18n/ja/hospital.lua
@@ -0,0 +1,19 @@
+return {
+    ja = {
+        ['hospital/nurse-1/message'] = "どうしました。どこがわるいんですか?",
+        ['hospital/nurse-1/telepathy'] = "わたしは　おおた。うるうる",
+        ['hospital/nurse-2/message'] = "にゅういんしている　かんじゃさんは、\nめんかいしゃぜつです。",
+        ['hospital/nurse-2/telepathy'] = "さいきん、くるーすりのそばでは\nたらくひとが　よくにゅういんしてくるわね。",
+        ['hospital/robot-1/message'] = "わたしは、かんごろぼっと。\nここは　びょういんです。",
+        ['hospital/robot-1/telepathy'] = "すぺーすころにーでの　たびも\nおしまいに　ちかずいている。\nあと　わずかで、あたらしいわくせいに　つく。",
+        ['hospital/guy-1/message'] = "なにかようかね",
+        ['hospital/guy-1/telepathy'] = "さいきっくたちがもっていた、\nゆかのみを　あげよう。",
+        ['hospital/guy-2/message'] = "もうひとりの、なかまのことを　ききたかったら、\nまっくすに　あってきなさい。",
+        ['hospital/guy-2/flaged'] = "そうか、まっくすは　しんでいたのか。\nおまえがさがしているなかまは、\nなたーしゃがしっている。",
+        ['hospital/guy-2/telepathy'] = "んんんんん",
+        ['hospital/citizen-1/message'] = "けけけけ、けけけ",
+        ['hospital/citizen-1/telepathy'] = "このせかいは、さいきっくたちいがいの\nべつの　ものたちのものに、なろうとしている。",
+        ['hospital/citizen-2/message'] = "うー、うー、うー",
+        ['hospital/citizen-2/telepathy'] = "わたしは、しりすぎて、はなしをできなくされた。\nくるーすりは、いでんしそうさを、\nおこなって　もんすたーを、たくさんつくっている。",
+    }
+}
