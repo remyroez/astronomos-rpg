0164f3efc9951a472233bf1e512dbc5ac046df10
れみろう <remyroez+github@gmail.com>
2018-04-02 22:38:08 +0900

    :sparkles: コマンド関連の i18n データを追加

---
+++ game/assets/i18n/ja/command.lua
@@ -0,0 +1,48 @@
+return {
+    ja = {
+        ['command/map/title'] = "COMMAND",
+        ['command/map/talk'] = "はなす",
+        ['command/map/medicine'] = "くすり",
+        ['command/map/strength'] = "ちから",
+        ['command/map/esp'] = "ESP",
+        ['command/map/discard'] = "すてる",
+        ['command/map/items'] = "もちもの",
+        
+        ['command/map/talk/no-npc'] = "だれも　いません。",
+        
+        ['command/map/medicine/no-medicine'] = "くすりを、もっていません。",
+
+        ['command/map/esp/title'] = "ESP",
+        ['command/map/esp/break'] = "ぶれいく",
+        ['command/map/esp/jump'] = "じゃんぷ",
+        ['command/map/esp/telepathy'] = "てれぱし",
+        ['command/map/esp/save'] = "SAVE",
+        
+        ['command/map/discard/no-item'] = "もちものが　ありません",
+        
+        ['command/map/strength/body'] = "からだ",
+        ['command/map/strength/body/normal'] = "ふつう",
+        ['command/map/strength/body/ko'] = "きぜつ",
+        ['command/map/strength/body/disease'] = "びょうき",
+        ['command/map/strength/body/dying'] = "かし",
+
+        ['command/map/strength/level'] = "れべる",
+        ['command/map/strength/hp'] = "ひっと力",
+        ['command/map/strength/max-hp'] = "MAXひっと力",
+        ['command/map/strength/pp'] = "さいこ力",
+        ['command/map/strength/max-pp'] = "MAXさいこ力",
+        ['command/map/strength/defence'] = "ぼうぎょ力",
+        ['command/map/strength/speed'] = "すばやさ",
+        ['command/map/strength/proficency'] = "じゅくれんど",
+        ['command/map/strength/total-exp'] = "けいけんち",
+        ['command/map/strength/next-exp'] = "NEXT",
+
+        ['command/map/esp/cant-use'] = "あなたは、つかえません。",
+        ['command/map/esp/save/message'] = "つぎのことばをおぼえておくのだ。",
+        ['command/map/esp/save/password'] = "　　　よいたびを　!",
+
+        ['command/map/items/title'] = "せんたく",
+        ['command/map/items/item'] = "あ　い　て　む",
+        ['command/map/items/ingredient'] = "くすりのもと",
+    }
+}
